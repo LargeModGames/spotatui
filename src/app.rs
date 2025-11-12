6cabcda96af3d0ea96e5f1411cdc9513aafdc7ed
LargeModGames <LargeModGames@gmail.com>
2025-11-12 20:15:11 +0100

    app: handle optional device volume_percent safely

--- src/app.rs
+++ src/app.rs
@@ -596,7 +596,7 @@ impl App {
 
   pub fn increase_volume(&mut self) {
     if let Some(context) = self.current_playback_context.clone() {
-      let current_volume = context.device.volume_percent as u8;
+      let current_volume = context.device.volume_percent.unwrap_or(0) as u8;
       let next_volume = min(
         current_volume + self.user_config.behavior.volume_increment,
         100,
@@ -610,7 +610,7 @@ impl App {
 
   pub fn decrease_volume(&mut self) {
     if let Some(context) = self.current_playback_context.clone() {
-      let current_volume = context.device.volume_percent as i8;
+      let current_volume = context.device.volume_percent.unwrap_or(0) as i8;
       let next_volume = max(
         current_volume - self.user_config.behavior.volume_increment as i8,
         0,
