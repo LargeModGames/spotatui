44dad86fa975435866ef83206f891717c4323c48
oud <d.japaryo@gmail.com>
2026-09-24 20:26:47 +0700

    refactor(requests): Removed access token logging

--- src/infra/network/requests.rs
+++ src/infra/network/requests.rs
@@ -334,7 +334,6 @@ where
       let token = token_lock
         .as_ref()
         .ok_or_else(|| anyhow!("No access token available"))?;
-      warn!("Access Token: {}", token.access_token);
       (token.access_token.clone(), token_age(token))
     };
 
@@ -504,7 +503,11 @@ impl Network {
     query: &[(&str, String)],
   ) -> anyhow::Result<T> {
     let debug_fail = env::var("MAKE_OUD_SUFFER").ok().is_none_or(|_| true);
-    if debug_fail && path.contains("/top-tracks") {
+    if debug_fail && (
+      path.contains("/top-tracks")
+      || path.contains("recommendations")
+      || path.contains("tracks?ids=")
+    ) {
       return Err(
         SpotifyApiError {
           status: StatusCode::FORBIDDEN,
